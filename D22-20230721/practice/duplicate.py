d08b7bc7d2766655636e5dbe986e1d6d0959fd79
sree-ram38 <sreeram2003ic@gmail.com>
2023-07-23 22:12:15 +0530

    upload

--- D22-20230721/practice/duplicate.py
+++ D22-20230721/practice/duplicate.py
@@ -1,4 +1,13 @@
 dup_val=[1,2,1,3,2,4,3,5,3,5,1,9,6,7]
+# val=["value"]
 # print(dup_val)
-res=set(dup_val)
-print(res)
+# res=set(dup_val)
+# print(res)
+# res=dict(zip(dup_val,val))
+# print(res)
+# list(OrderedDict.fromkeys(dup_val).keys())
+original=[]
+for dup in dup_val:
+    if dup not in original:
+        original.append(dup)
+print("original value : ",original)
